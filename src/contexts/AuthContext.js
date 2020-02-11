96d50ff89185b6abc396954440f8cb2776037eb8
Christopher Lawson <c.lawson@barclaycomms.com>
2020-02-11 13:32:55 +0000

    added authContext

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -6,9 +6,17 @@ class AuthContextProvider extends Component {
     state = {
         isAuthenticated: false
     }
-
+    toggleAuth = () => {
+        this.setState({
+            isAuthenticated: !this.state.isAuthenticated
+        })
+    }
     render() {
-        return (<div></div>);
+        return (
+            <AuthContext.Provider value={{...this.state, toggleAuth: this.toggleAuth}}>
+                {this.props.children}
+            </AuthContext.Provider>
+        );
     }
 }
 
