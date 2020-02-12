30ae3129dac0dc038ea230308b0c25ea5f542231
LawsonUK <chrislawsonuk@hotmail.com>
2020-02-12 21:44:23 +0000

    Fixed context of book list component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,26 +2,22 @@ import React, { Component } from "react"
 import { ThemeContext } from "../contexts/ThemeContext"
 
 export class BookList extends Component {
+  static contextType = ThemeContext
+
   render() {
+    const { isLightTheme, light, dark } = this.context
+    const theme = isLightTheme ? light : dark
     return (
-      <ThemeContext.Consumer>
-        {themeContext => {
-          const { isLightTheme, light, dark } = themeContext
-          const theme = isLightTheme ? light : dark
-          return (
-            <div
-              className="book-list"
-              style={{ color: theme.syntax, background: theme.bg }}
-            >
-              <ul>
-                <li style={{ background: theme.ui }}>The Way of Kings</li>
-                <li style={{ background: theme.ui }}>The Name of the Wind</li>
-                <li style={{ background: theme.ui }}>The Final Empire</li>
-              </ul>
-            </div>
-          )
-        }}
-      </ThemeContext.Consumer>
+      <div
+        className="book-list"
+        style={{ color: theme.syntax, background: theme.bg }}
+      >
+        <ul>
+          <li style={{ background: theme.ui }}>The Way of Kings</li>
+          <li style={{ background: theme.ui }}>The Name of the Wind</li>
+          <li style={{ background: theme.ui }}>The Final Empire</li>
+        </ul>
+      </div>
     )
   }
 }
